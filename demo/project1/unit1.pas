610e2fccc18f9e2add0dac2cebac9eebca6eda40
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-01-09 09:36:56 +0000

    zmsql: Initial commit to ccr-trunk based on http://forum.lazarus.freepascal.org/index.php/topic,38705.msg270934.html#msg270934
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn/components/zmsql@6119 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ demo/project1/unit1.pas
@@ -0,0 +1,74 @@
+unit Unit1; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  DbCtrls, DBGrids, ZMConnection, ZMQueryDataSet, db;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Datasource1: TDatasource;
+    DBGrid1: TDBGrid;
+    DBNavigator1: TDBNavigator;
+    ZMConnection1: TZMConnection;
+    ZMQueryDataSet1: TZMQueryDataSet;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  db: String;
+begin
+  db := ZMQueryDataset1.ZMConnection.DatabasePath + ZMQueryDataset1.TableName + '.csv';
+  ShowMessage('Data is going to be loaded from: '+ db);
+  ZMQueryDataset1.LoadFromTable;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  vOriginalTableName:string;
+begin
+  try
+    vOriginalTableName:=ZMQueryDataset1.TableName;
+    ZMQueryDataset1.TableName:='Test';
+    ShowMessage('Dataset is going to be saved to: '+
+       ZMQueryDataset1.ZMConnection.DatabasePath +
+       ZMQueryDataset1.TableName+'.csv');
+    ZMQueryDataset1.SaveToTable(FormatSettings.DecimalSeparator);
+  finally
+    ZMQueryDataset1.TableName:=vOriginalTableName;
+  end;
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ZMConnection1.DatabasePath := '..\data\';
+end;
+
+
+end.
+
